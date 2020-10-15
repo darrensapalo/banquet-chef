9fe1ca6b0d563d11154f132b0b704fbe715a7e4b
Darren Sapalo <darren.sapalo@gmail.com>
2020-10-15 21:52:58 +0800

    Added sample creds from google. Added cross origin header support.

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const { GoogleSpreadsheet } = require('google-spreadsheet');
  
 // spreadsheet key is the long id in the sheets URL
 
-exports.helloWorld = async (req, res) => {
-    
-    console.log("1")
+exports.contactTrace = async (req, res) => {
+    process.env.GOOGLE_SPREADSHEET_ID = process.env.GOOGLE_SPREADSHEET_ID || "1aZELho45USPIxhXysshkApzPxWaCkXqCSJ0EFhLLuNQ";
+    console.log("Loading Google Sheet: " + process.env.GOOGLE_SPREADSHEET_ID)
     const doc = new GoogleSpreadsheet(process.env.GOOGLE_SPREADSHEET_ID);
 
     // use service account creds
@@ -13,7 +13,7 @@ exports.helloWorld = async (req, res) => {
     //     private_key: process.env.GOOGLE_PRIVATE_KEY,
     // });
     // // OR load directly from json file if not in secure environment
-    console.log("2")
+    
     
     await doc.useServiceAccountAuth(require('./creds-from-google.json'));
     // // OR use service account to impersonate a user (see https://developers.google.com/identity/protocols/oauth2/service-account#delegatingauthority)
@@ -22,7 +22,6 @@ exports.helloWorld = async (req, res) => {
     // doc.useApiKey('YOUR-API-KEY');
     
 
-    console.log("3")
     try {
         await doc.loadInfo(); // loads document properties and worksheets
     } catch (e) {
@@ -31,25 +30,69 @@ exports.helloWorld = async (req, res) => {
         return;
     }
 
-    console.log("4")
-    console.log(doc.title);
-    await doc.updateProperties({ title: 'renamed doc' });
-    
-    const attendanceSheet = doc.sheetsByTitle["attendances"];
-    console.log(attendanceSheet.title);
-    console.log(attendanceSheet.rowCount);
-    
-    console.log("Request")
-    console.log(req.query);
+    const allowedOrigins = [
+        'https://olfp-makati.netlify.app',
+        'http://localhost:3000'
+    ]
 
-    const userID = req.query.userid;
+    const origin = req.header('Origin');
+    if (allowedOrigins.includes(origin)) {
+        res.set('Access-Control-Allow-Origin', origin);
+    }
+    res.set('Access-Control-Allow-Credentials', 'true');
 
-    const date = new Date().toLocaleString('en-PH', { options: { dateStyle: 'full', timeStyle: 'full' }});
-    console.log(date);
+    const traceUserId = req.query.traceUserId;
+    const getUserId = req.query.getUserId;
 
-    const row = await attendanceSheet.addRow([
-        date, userID, 'entry', 'valid'
-    ])
+    if (traceUserId) {
+        const attendanceSheet = doc.sheetsByTitle["attendances"];
     
-    res.send("Successfully inserted");
+        const date = new Date().toString('en-PH', { options: { dateStyle: 'full', timeStyle: 'full' }});
+        console.log(date);
+
+        const row = await attendanceSheet.addRow([
+            date, traceUserId, 'entry', 'valid'
+        ])
+        
+
+        res.send("Successfully inserted");
+    } else if (getUserId) {
+        console.log("Getting user" + getUserId)
+        const usersSheet = doc.sheetsByTitle["users"];
+            
+        const rows = await usersSheet.getRows()
+        console.log("Got rows" + rows.length);
+
+        const responses = rows.map(row => ({
+            user_id: row.user_id,
+            name: row.name,
+            address: row.address,
+            mobile_number: row.mobile_number
+        }));
+
+        const response = responses.find(r => r.user_id === getUserId)
+
+        if (response) {
+            res.send(JSON.stringify(response, null, 2));
+        } else {
+            res.status(404).send(JSON.stringify({
+                "message": "not found"
+            }, null, 2));
+        }
+    } else {
+        console.log("Getting attendances")
+        const attendanceSheet = doc.sheetsByTitle["attendances"];
+            
+        const rows = await attendanceSheet.getRows()
+        console.log("Got total attendance count: " + rows.length);
+
+        const responses = rows.map(row => ({
+            created_at: row.created_at,
+            user_id: row.user_id,
+            type: row.type,
+            status: row.status
+        }));
+
+        res.send(JSON.stringify(responses, null, 2));
+    }
   };
